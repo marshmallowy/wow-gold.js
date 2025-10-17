b78b3031b962e66a3618747a705471f1de616446
marshmallowy <marshmallowy-mail@protonmail.com>
2025-10-17 22:50:45 +0100

    added index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+export * from './error/InfiniteValueError';
+export * from './error/MalformedGoldExpressionError';
+export * from './error/NonIntegerValueError';
+export * from './error/UnsafeNumberError';
+export * from './error/ValueNaNError';
+
+export * from './gold-expression';
+export * from './gold';
